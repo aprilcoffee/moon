c3d337105a238c6f49aa3f264465b7e3001ed396
aprilcoffee <8765498@gmail.com>
2021-04-23 12:13:59 +0200

    update

--- sketch.js
+++ sketch.js
@@ -6,39 +6,35 @@ let myFont;
 let system;
 
 let camTarget;
-function preload(){
-	myFont = loadFont ('assets/sans.otf');
 
+function preload() {
+	myFont = loadFont('assets/sans.otf');
 }
-window.addEventListener('deviceorientation', (event) => {
-	// Expose each orientation angle in a more readable way
-	rotation_degrees = event.alpha;
-	frontToBack_degrees = event.beta;
-	leftToRight_degrees = event.gamma;
 
-	// Update velocity according to how tilted the phone is
-	// Since phones are narrower than they are long, double the increase to the x velocity
-	//vx = vx + leftToRight_degrees * updateRate * 2;
-	//vy = vy + frontToBack_degrees * updateRate;
+function onOrientationChange(e) {
+	alpha = e.alpha;
+	beta = e.beta;
+	gamma = e.gamma;
 
-
-	//dot = document.getElementsByClassName("indicatorDot")[0]
-	//dot.setAttribute('style', "left:" + (px) + "%;" +
-	//	"top:" + (py) + "%;");
-
+	println(alpha + " " + beta + " " + gamma);
 }
+
+
 function setup() {
-	const canvasElt = createCanvas(400,600,WEBGL).elt;
-	canvasElt.style.width='100%', canvasElt.style.height='100%';
+	const canvasElt = createCanvas(400, 600, WEBGL).elt;
+	canvasElt.style.width = '100%', canvasElt.style.height = '100%';
 	//	createCanvas(displayWidth, displayHeight, WEBGL);
-  	backGroundColorTop = loadImage("img/oceanAbove.jpg");
-  	backGroundColorDown = loadImage("img/oceanBelow.jpg");
-  	backGroundColorGradient = loadImage("img/oceanBackground.jpg");
+	backGroundColorTop = loadImage("img/oceanAbove.jpg");
+	backGroundColorDown = loadImage("img/oceanBelow.jpg");
+	backGroundColorGradient = loadImage("img/oceanBackground.jpg");
 	textFont(myFont);
+	if (window.DeviceOrientationEvent) {
+		window.addEventListener('deviceorientation', onOrientationChange);
+	}
 	//system = new ParticleSystem(createVector(0,5,10));
 	//system = new particle
 
-	camTarget=createVector(0,0,0);
+	camTarget = createVector(0, 0, 0);
 }
 
 function draw() {
@@ -46,34 +42,34 @@ function draw() {
 	//fill(255);
 	//text('p5*js', 10, 50);
 
-	fill(0,0,0,0);
+	fill(0, 0, 0, 0);
 	//let angleX = map(mouseX,0,displayWidth,-179,179);
 	//let angleZ = map(mouseY,0,displayHeight,-45,45);
-	let angle_t = map(constrain(pRotationX,0,180),0,180,89,-89);
-	let angle_a = map(constrain(pRotationY,0,180),0,180,-89,89);
+	let angle_t = map(constrain(pRotationX, 0, 180), 0, 180, 89, -89);
+	let angle_a = map(constrain(pRotationY, 0, 180), 0, 180, -89, 89);
 	let fixR = 100;
 
-	let camX = fixR*cos(radians(angle_a))*cos(radians(angle_t));
-	let camY = fixR*sin(radians(angle_t));
-	let camZ = fixR*sin(radians(angle_a))*cos(radians(angle_t));
-	camTarget=createVector(camX,camY,camZ);
-	camera(0, 0, 0, camX, camY,camZ, 0, 1, 0);
+	let camX = fixR * cos(radians(angle_a)) * cos(radians(angle_t));
+	let camY = fixR * sin(radians(angle_t));
+	let camZ = fixR * sin(radians(angle_a)) * cos(radians(angle_t));
+	camTarget = createVector(camX, camY, camZ);
+	camera(0, 0, 0, camX, camY, camZ, 0, 1, 0);
 
 	//system.show();
 	push();
-	translate(0,5,5);
+	translate(0, 5, 5);
 	textSize(12);
 	fill(255);
-	text(pRotationX,0,-10);
-	text(pRotationY,0,0);
-	text(pRotationZ,0,10);
+	text(pRotationX, 0, -10);
+	text(pRotationY, 0, 0);
+	text(pRotationZ, 0, 10);
 
 	pop();
 
 	texture(backGroundColorGradient);
 	textureMode(NORMAL);
 	noStroke();
-	translate(500,0);
+	translate(500, 0);
 	sphere(1000);
 
 	//shakeCheck();
@@ -81,46 +77,47 @@ function draw() {
 
 
 function onClick() {
-// feature detect
+	// feature detect
 	if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-	DeviceOrientationEvent.requestPermission().then(permissionState => {
-		if (permissionState === 'granted') {
-			window.addEventListener('deviceorientation', () => {});}
+		DeviceOrientationEvent.requestPermission().then(permissionState => {
+			if (permissionState === 'granted') {
+				window.addEventListener('deviceorientation', () => {});
+			}
 		}).catch(console.error);
 	} else {
-	// handle regular non iOS 13+ devices
+		// handle regular non iOS 13+ devices
 	}
 }
-function touchEnded(){
+
+function touchEnded() {
 	var spk = new p5.Speech();
-	var currentText = char(int(97+random(26)));
+	var currentText = char(int(97 + random(26)));
 	//system.add(camTarget,currentText);
 	spk.speak(currentText);
 }
 
 function shakeCheck() {
 
-  var shakeThreshold = 50;
-  accX = abs(accelerationX - pAccelerationX);
-  accY = abs(accelerationY - pAccelerationY);
-  accT = accX + accY;
-  // If shake
-  if (accT >= shakeThreshold) {
-		  var spk = new p5.Speech();
-		  var currentText = char(int(97+random(26)));
-		  //system.add(camTarget,currentText);
-		  spk.speak(currentText);
-  }
-  // If not shake
-  else {
-  }
+	var shakeThreshold = 50;
+	accX = abs(accelerationX - pAccelerationX);
+	accY = abs(accelerationY - pAccelerationY);
+	accT = accX + accY;
+	// If shake
+	if (accT >= shakeThreshold) {
+		var spk = new p5.Speech();
+		var currentText = char(int(97 + random(26)));
+		//system.add(camTarget,currentText);
+		spk.speak(currentText);
+	}
+	// If not shake
+	else {}
 }
 
 
 
 
 //Prevent mouse dragging event
-function touchMoved(event){
+function touchMoved(event) {
 	console.log(event);
 	return false;
 }
